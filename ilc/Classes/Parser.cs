d251da61f44ca8f26e602532cbfd467663170989
IamInsane0xd <biczo.ferenc001@gmail.com>
2021-12-29 14:43:47 +0100

    Move precedencies to SyntaxFacts

--- ilc/Classes/Parser.cs
+++ ilc/Classes/Parser.cs
@@ -68,7 +68,7 @@ namespace ILang.Classes
 
 			while (true)
 			{
-				var precedence = GetBinaryOperatorPrecedence(Current.Kind);
+				var precedence = Current.Kind.GetBinaryOperatorPrecedence();
 				
 				if (precedence == 0 || precedence <= parentPrecedence)
 					break;
@@ -80,23 +80,6 @@ namespace ILang.Classes
 			}
 
 			return left;
-		}
-
-		private static int GetBinaryOperatorPrecedence(SyntaxKind kind)
-		{
-			switch (kind)
-			{
-				case SyntaxKind.StarToken:
-				case SyntaxKind.SlashToken:
-					return 2;
-
-				case SyntaxKind.PlusToken:
-				case SyntaxKind.MinusToken:
-					return 1;
-				
-				default:
-					return 0;
-			}
 		}
 
 		private ExpressionSyntax ParsePrimaryExpression()
