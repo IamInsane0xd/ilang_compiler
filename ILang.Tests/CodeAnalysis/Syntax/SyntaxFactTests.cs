5e9e26b854a08bfe7e77756d38cd774b213bb115
IamInsane0xd <biczo.ferenc001@gmail.com>
2021-12-30 16:33:35 +0100

    expose SyntaxFacts.GetText() and add SyntaxFacts tests

---
+++ ILang.Tests/CodeAnalysis/Syntax/SyntaxFactTests.cs
@@ -0,0 +1,33 @@
+﻿using ILang.CodeAnalysis.Syntax;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ILang.Tests.CodeAnalysis.Syntax;
+
+public class SyntaxFactTests
+{
+	[Theory]
+	[MemberData(nameof(GetSyntaxKindData))]
+	public void SyntaxFactGetTextRoundTrips(SyntaxKind kind)
+	{
+		var text = SyntacFacts.GetText(kind);
+
+		if (text == null)
+			return;
+
+		var tokens = SyntaxTree.ParseTokens(text);
+		var token = Assert.Single(tokens);
+
+		Assert.Equal(kind, token.Kind);
+		Assert.Equal(text, token.Text);
+	}
+
+	public static IEnumerable<object[]> GetSyntaxKindData()
+	{
+		var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+
+		foreach (var kind in kinds)
+			yield return new object[] { kind }; 
+	}
+}
--- ILang/CodeAnalysis/Syntax/SyntacFacts.cs
+++ ILang/CodeAnalysis/Syntax/SyntacFacts.cs
@@ -1,6 +1,6 @@
 namespace ILang.CodeAnalysis.Syntax;
 
-internal static class SyntacFacts
+public static class SyntacFacts
 {
 	public static int GetUnaryOperatorPrecedence(this SyntaxKind kind)
 	{
@@ -55,5 +55,55 @@ internal static class SyntacFacts
 		}
 
 		return SyntaxKind.IdentifierToken;
+	}
+
+	public static string? GetText(SyntaxKind kind)
+	{
+		switch (kind)
+		{
+			case SyntaxKind.PlusToken:
+				return "+";
+
+			case SyntaxKind.MinusToken:
+				return "-";
+
+			case SyntaxKind.StarToken:
+				return "*";
+
+			case SyntaxKind.SlashToken:
+				return "/";
+
+			case SyntaxKind.BangToken:
+				return "!";
+
+			case SyntaxKind.EqualsToken:
+				return "=";
+
+			case SyntaxKind.AmpersandAmpersandToken:
+				return "&&";
+
+			case SyntaxKind.PipePipeToken:
+				return "||";
+
+			case SyntaxKind.EqualsEqualsToken:
+				return "==";
+
+			case SyntaxKind.BangEqualsToken:
+				return "!=";
+
+			case SyntaxKind.OpenParenthesisToken:
+				return "(";
+
+			case SyntaxKind.CloseParenthesisToken:
+				return ")";
+
+			case SyntaxKind.FalseKeyword:
+				return "false";
+
+			case SyntaxKind.TrueKeyword:
+				return "true";
+		}
+
+		return null;
 	}
 }
